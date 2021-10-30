c2687334d16b18ef7ca67b860851dee741d270a3
Rishav-hub <vibhanshuchauhan200@gmail.com>
2021-10-30 09:03:30 +0530

    created prepared folder

--- src/stage_01_prepare.py
+++ src/stage_01_prepare.py
@@ -3,7 +3,8 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.all_utils import read_yaml
+from src.utils.all_utils import read_yaml, create_directory
+import random
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
@@ -12,8 +13,29 @@ logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=lo
                     filemode="a")
 
 def main(config_path, params_path):
+    """
+    Main function for stage_01_prepare.py to convert xml to tsv
+    :param config_path: path to config file
+    :config_path type: str
+    """
     config = read_yaml(config_path)
     params = read_yaml(params_path)
+
+    source_data = config['source_data']
+    input_data = os.path.join(source_data['data_dir'], source_data['data_file'])
+
+    split = params['prepare']['split']
+    seed = params['prepare']['seed']
+
+    random.seed(seed)
+
+    artifacts = config['artifacts']
+    prepare_data_dir_path = os.path.join(artifacts['ARTIFACTS_DIR'], artifacts['PREPARED_DATA'])
+
+    create_directory([prepare_data_dir_path])
+
+
+
 
     
 
@@ -27,7 +49,7 @@ if __name__ == '__main__':
     try:
         logging.info("\n********************")
         logging.info(">>>>> stage one started <<<<<")
-        main(config_path=parsed_args.config)
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
         logging.info(">>>>> stage one completed! all the data are saved in local <<<<<n")
     except Exception as e:
         logging.exception(e)
