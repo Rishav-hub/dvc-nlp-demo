fb7fb7b5a96177120e78722b496470759d2b2c96
Rishav-hub <vibhanshuchauhan200@gmail.com>
2021-10-30 10:16:44 +0530

    updated stage 2 featurization

--- src/stage_02_featurization.py
+++ src/stage_02_featurization.py
@@ -3,8 +3,9 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.all_utils import read_yaml, create_directory
+from src.utils.all_utils import read_yaml, create_directory, get_df
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 STAGE = "Two"
 
@@ -30,7 +31,14 @@ def main(config_path, params_path):
     featurized_train_data_path = os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_OUT_TRAIN"])
     featurized_test_data_path = os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_OUT_TEST"])    
 
+    max_features = params["featurize"]["max_features"]
+    ngrams = params["featurize"]["ngrams"]
 
+    df_train = get_df(train_data_path)
+
+    train_words = np.array(df_train.text.str.lower().values.astype("U"))
+
+    print(train_words[:20])
 
 
 if __name__ == '__main__':
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -2,6 +2,7 @@ import yaml
 import os
 import json
 import time
+import logging
 
 def read_yaml(path_to_yaml: str) -> dict:
     """
@@ -40,3 +41,14 @@ def get_timestamp(name):
     timestamp = time.asctime().replace(" ", "_").replace(":", "_")
     unique_name = f"{name}_at_{timestamp}"
     return unique_name
+
+def get_df(path_to_data: str, sep: str="\t") -> pd.DataFrame:
+    df = pd.read_csv(
+        path_to_data,
+        encoding="utf-8",
+        header=None,
+        delimiter=sep,
+        names=["id", "label", "text"],
+    )
+    logging.info(f"The input data frame {path_to_data} size is {df.shape}\n")
+    return df
